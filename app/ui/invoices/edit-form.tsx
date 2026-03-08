66e3dc20b286bc2b10fba182564a2d152fab8c1f
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2026-03-08 15:48:51 +0200

    Fix action errors

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,7 +22,12 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
 
   return (
-    <form action={updateInvoiceWithId}>
+    // { message: null, errors: {} }
+    <form
+      action={async (formData: FormData) => {
+        await updateInvoice(invoice.id, formData)
+      }}
+    >
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
